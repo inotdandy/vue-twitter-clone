4f941b8ff9412e23404fa62efa3dbe608de44d9e
Dandy Inot <dandy.inot1990@gmail.com>
2021-09-11 10:43:19 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    @{{user.username}} - {{ fullName }} Follwers {{ followUser() }}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
+  data(){
+    return {
+      followers: 0,
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+        username: 'johnD',
+        email: 'johndoe@test.com',
+        isAdmin: true
+      }
+    }
+  },
+  computed: {
+    fullName(){
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  methods:{
+    followUser(){
+      return this.followers
+    }
+  },
   components: {
-    HelloWorld
+   
   }
 }
 </script>
